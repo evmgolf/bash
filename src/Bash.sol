02c105be33eadf1d3b74571878fd2ad09c7ea8fa
evmgolf <evmgolf@github.com>
2022-06-08 19:56:13 +0100

    Added a script() function which does not output to a file.

--- src/Bash.sol
+++ src/Bash.sol
@@ -5,14 +5,18 @@ import {Script} from "forge-std/Script.sol";
 import {Decimal} from "codec/Decimal.sol";
 
 contract Bash is Script {
+  function run(bytes memory script) external returns (bytes memory) {
+    string[] memory args = new string[](3);
+    args[0] = "bash";
+    args[1] = "-c";
+    args[2] = string(script);
+    return vm.ffi(args);
+  }
   function run(bytes memory script, bytes memory filename) external returns (bytes memory) {
     string[] memory args = new string[](3);
     args[0] = "bash";
     args[1] = "-c";
-    if (filename.length > 0) {
-      script = bytes.concat(script, " >", filename);
-    }
-    args[2] = string(script);
+    args[2] = string(bytes.concat(script, " >", filename));
     return vm.ffi(args);
   }
 }
